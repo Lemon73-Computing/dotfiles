e8aca188997413c4503c3ad048d25044cb27a84e
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-08-24 16:47:10 +0900

    chore (copyright-text): create shell file to get 2024 copyright text

---
+++ docs/copyright-text_japan-gov-bunka.sh
@@ -0,0 +1,11 @@
+# ---
+# Creator: 文化庁 (Agency for Cultural Affairs. Government of Japan)
+# Creator top URL: https://www.bunka.go.jp (https://www.bunka.go.jp/english/)
+# ---
+# Doc Name: 令和6年度著作権テキスト (2024 Copyright Text)
+# Doc Original URL: https://www.bunka.go.jp/seisaku/chosakuken/textbook/
+# License: (Original License and Read below link)
+# Detail Rules: https://www.bunka.go.jp/seisaku/chosakuken/textbook/
+# ---
+
+wget https://www.bunka.go.jp/seisaku/chosakuken/textbook/pdf/94089201_01.pdf -O copyright-text_japan-gov-bunka.pdf
